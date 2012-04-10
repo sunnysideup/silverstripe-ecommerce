606265acffbdf7044c70730008d227d530d94c83
Nicolaas <github@sunnysideup.co.nz>
2012-04-10 11:35:12 +0000

    MINOR
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@2422 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/money/CurrencyImprovements.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This class adds some formatting improvements to e-commerce.
+ *
+ *@author Nicolaas [at] sunny side up . co . nz
+ */
+
+
+
+class CurrencyImprovements extends Currency {
+
+
+
+}
