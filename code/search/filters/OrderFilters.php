c2f16fa0751974dc1285316ea1d61ff3a2d451e5
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-04-11 07:51:59 +0000

    API: improved searching for orders
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@2432 950b781c-c544-efab-2f99-90083ee69683

--- code/search/filters/OrderFilters.php
+++ code/search/filters/OrderFilters.php
@@ -13,9 +13,17 @@
 
 class OrderFilters_AroundDateFilter extends ExactMatchFilter {
 
-	protected static $how_many_days_around = 31;
-		static function set_how_many_days_around($i){self::$how_many_days_around = $i;}
-		static function get_how_many_days_around(){return self::$how_many_days_around;}
+	/**
+	 * The divider is used to work out the
+	 * maximum number of days we should be from the date.
+	 * The Further back in time we go, the greater the margin of error.
+	 *
+	 * For example, if you search for a date that is one year ago,
+	 * then the margin of error is 360/12 = 30 days.
+	 *
+	 * @var Int
+	 */
+	private $divider = 12;
 
 	/**
 	 *
@@ -26,19 +34,94 @@ class OrderFilters_AroundDateFilter extends ExactMatchFilter {
 		$value = $this->getValue();
 		$date = new Date();
 		$date->setValue($value);
+		$distanceFromToday = time() - strtotime($value);
+		$maxDays = round($distanceFromToday/($this->divider * 86400))+1;
 		$formattedDate = $date->format("Y-m-d");
-
 		// changed for PostgreSQL compatability
 		// NOTE - we may wish to add DATEDIFF function to PostgreSQL schema, it's just that this would be the FIRST function added for SilverStripe
 		$db = DB::getConn();
 		if( $db instanceof PostgreSQLDatabase ) {
 			// don't know whether functions should be used, hence the following code using an interval cast to an integer
-			$query->where("(\"Order\".\"Created\"::date - '$formattedDate'::date)::integer > -".self::get_how_many_days_around()." AND (\"Order\".\"Created\"::date - '$formattedDate'::date)::integer < ".self::get_how_many_days_around());
+			$query->where("(\"Order\".\"Created\"::date - '$formattedDate'::date)::integer > -".$maxDays." AND (\"Order\".\"Created\"::date - '$formattedDate'::date)::integer < ".$maxDays);
 		}
 		else {
 			// default is MySQL DATEDIFF() function - broken for others, each database conn type supported must be checked for!
-			$query->where("(DATEDIFF(\"Order\".\"Created\", '$formattedDate') > -".self::get_how_many_days_around()." AND DATEDIFF(\"Order\".\"Created\", '$formattedDate') < ".self::get_how_many_days_around().")");
+			$query->where("(DATEDIFF(\"Order\".\"Created\", '$formattedDate') > -".$maxDays." AND DATEDIFF(\"Order\".\"Created\", '$formattedDate') < ".$maxDays.")");
 		}
+		return $query;
+
+	}
+
+	/**
+	 *
+	 *@return Boolean
+	 **/
+	public function isEmpty() {
+		return $this->getValue() == null || $this->getValue() == '';
+	}
+
+}
+
+/**
+ * Filter that searches the Two Addresses (billing + shipping)
+ * and the member. It searches all the relevant fields.
+ *
+ *
+ */
+
+class OrderFilters_MemberAndAddress extends ExactMatchFilter {
+
+	/**
+	 *
+	 *@return SQLQuery
+	 **/
+	public function apply(SQLQuery $query) {
+		$query = $this->applyRelation($query);
+		$value = $this->getValue();
+		$billingAddressesIDs = array(-1 => -1);
+		$billingAddresses = DataObject::get("BillingAddress", "
+			\"FirstName\" LIKE '%$value%' OR
+			\"Surname\" LIKE '%$value%' OR
+			\"Email\" LIKE '%$value%' OR
+			\"Address\" LIKE '%$value%' OR
+			\"Address2\" LIKE '%$value%' OR
+			\"City\" LIKE '%$value%' OR
+			\"PostalCode\" LIKE '%$value%' OR
+			\"Phone\" LIKE '%$value%' OR
+			\"MobilePhone\" LIKE '%$value%'
+
+		");
+		if($billingAddresses) {
+			$billingAddressesIDs = $billingAddresses->map("ID", "ID");
+		}
+		$where[] = "\"BillingAddressID\" IN (".implode(",", $billingAddressesIDs).")";
+		$shippingAddressesIDs = array(-1 => -1);
+		$shippingAddresses = DataObject::get("ShippingAddress", "
+			\"ShippingFirstName\" LIKE '%$value%' OR
+			\"ShippingSurname\" LIKE '%$value%' OR
+			\"ShippingAddress\" LIKE '%$value%' OR
+			\"ShippingAddress2\" LIKE '%$value%' OR
+			\"ShippingCity\" LIKE '%$value%' OR
+			\"ShippingPostalCode\" LIKE '%$value%' OR
+			\"ShippingPhone\" LIKE '%$value%' OR
+			\"ShippingMobilePhone\" LIKE '%$value%'
+
+		");
+		if($shippingAddresses) {
+			$shippingAddressesIDs = $shippingAddresses->map("ID", "ID");
+		}
+		$where[] = "\"ShippingAddressID\" IN (".implode(",", $shippingAddressesIDs).")";
+		$memberIDs = array(-1 => -1);
+		$members = DataObject::get("Member", "
+			\"FirstName\" LIKE '%$value%' OR
+			\"Surname\" LIKE '%$value%' OR
+			\"Email\" LIKE '%$value%'
+		");
+		if($members) {
+			$memberIDs = $members->map("ID", "ID");
+		}
+		$where[] = "\"MemberID\" IN (".implode(",", $memberIDs).")";
+		$query->where("(".implode(") OR (", $where).")");
 		return $query;
 
 	}
