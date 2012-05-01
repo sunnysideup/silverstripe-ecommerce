49208539a2f4e0c33dbc106e959ffff38ffe4550
Nicolaas <github@sunnysideup.co.nz>
2012-05-01 10:21:25 +0000

    MINOR

--- code/dod/EcommerceSiteTreeExtension.php
+++ code/dod/EcommerceSiteTreeExtension.php
@@ -52,6 +52,9 @@ class EcommerceSiteTreeExtension_Controller extends Extension {
 	 **/
 	function onBeforeInit() {
 		Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
+		//Requirements::block(THIRDPARTY_DIR."/jquery/jquery.js");
+		//Requirements::javascript(Director::protocol()."ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
+		//todo: check if we even need this (via ShoppingCartsRequirements.ss)		
 		Requirements::javascript("ecommerce/javascript/EcomCart.js");
 	}
 
