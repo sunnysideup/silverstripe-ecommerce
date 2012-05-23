02df28070d193380ae4857d1906da7b12aed2b97
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-05-23 23:14:55 +0000

    ADDING
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@2674 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/tasks/EcommerceTemplateTestTask.php
@@ -0,0 +1,14 @@
+<?php
+
+class EcommerceTemplateTestTask extends BuildTask{
+
+	protected $title = "Get help with building templates";
+
+	protected $description = "shows you some of the variables and controls you can use in your template";
+
+	function run($request){
+		DB::alteration_message("<br /><br /><br /><br /><br /><br /><a href=\"/ecommercetemplatetest/?flush=all\" target=\"_debug\">click here to view template test page</a>.<br /><br /><br /><br /><br /><br />");
+	}
+
+}
+
