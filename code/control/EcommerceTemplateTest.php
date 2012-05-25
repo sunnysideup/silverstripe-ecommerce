d1992d0d6f2101dd34fd0e6a461a0607dea10a18
Nicolaas <github@sunnysideup.co.nz>
2012-05-25 03:29:55 +0000

    API: adding Random Product
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@2687 950b781c-c544-efab-2f99-90083ee69683

--- code/control/EcommerceTemplateTest.php
+++ code/control/EcommerceTemplateTest.php
@@ -2,5 +2,13 @@
 
 class EcommerceTemplateTest extends Page_Controller {
 
+	function RandomProduct(){
+		$products = DataObject::get("Product", "\"AllowPurchase\" = 1  AND \"Price\" > 0", "RAND()", null, 100);
+		foreach($products as $product) {
+			if($product->canPurchase()) {
+				return $product;
+			}
+		}
+	}
 
 }
