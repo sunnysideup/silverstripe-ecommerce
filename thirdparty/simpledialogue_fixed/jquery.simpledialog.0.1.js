f8e9a56d3bee87d5eba91242070c26bfad271a75
Nicolaas <github@sunnysideup.co.nz>
2012-05-25 06:49:56 +0000

    MINOR

--- thirdparty/simpledialogue_fixed/jquery.simpledialog.0.1.js
+++ thirdparty/simpledialogue_fixed/jquery.simpledialog.0.1.js
@@ -213,7 +213,7 @@
 								.css({
 									position: 'absolute',
 									top: 0,
-									left: (w-scObj.width()) + 'px',
+									right: '18px',
 									opacity: 0.85
 								})
 								.find('a').click(jQuery.simpleDialog.close);
