20e6be59830eacdb485fe6084e8aeeb1baf301d3
Nicolaas <github@sunnysideup.co.nz>
2012-06-10 04:37:01 +0000

    MINOR: nicer feedback message in ecommerce/code/tasks/CartCleanupTask.php
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@2785 950b781c-c544-efab-2f99-90083ee69683

--- code/tasks/CartCleanupTask.php
+++ code/tasks/CartCleanupTask.php
@@ -69,7 +69,10 @@ class CartCleanupTask extends HourlyTask {
 			if($this->verbose) {
 				$totalToDeleteSQLObject = DB::query("SELECT COUNT(*) FROM \"Order\" $join WHERE $where");
 				$totalToDelete = $totalToDeleteSQLObject->value();
-				DB::alteration_message("<h2>Total number of abandonned carts: ".$totalToDelete." .... <br />now deleting: ".$maximumNumberOfObjectsDeleted." <br />Criteria: ".$clearMinutes." minutes ago or more.</h2>", "created");
+				DB::alteration_message("
+					<h2>Total number of abandonned carts: ".$totalToDelete."</h2>
+					<br />now deleting: ".$maximumNumberOfObjectsDeleted."
+					<br />Criteria: last edited ".$clearMinutes." minutes ago or more and not linked to a member", "created");
 				if($neverDeleteIfLinkedToMember) {
 					DB::alteration_message("<h3>Carts linked to a member will NEVER be deleted.</h3>", "edited");
 				}
