b57b756db54116fdbe3fbc9ebdc0c01af4a1ba3c
Nicolaas <github@sunnysideup.co.nz>
2012-06-24 23:54:33 +0000

    Moving location of API files

--- code/api/EcommerceRestfulServer.php
+++ code/api/EcommerceRestfulServer.php
@@ -1,12 +1,15 @@
 <?php
 /**
  * extends the standard RestfulServer to:
- * - provide better access to extended classes  
+ * - provide better access to extended classes
+ *
+ * see: http://api.silverstripe.org/2.4/sapphire/api/RestfulServer.html
+ *
+ * YOu can show JSON by hacking: RestfulServer::getDataFormatter
+www * NOTE: JSON IS NOT AVAILABLE YET WITHIN RESTFUL SERVER
  *
  *
- *
- *
- */ 
+ */
 
 
 
@@ -14,25 +17,25 @@
 
 
 class EcommerceRestfulServer extends RestfulServer {
-	
+
 	function index() {
-		XMLDataFormatter::$api_base = 'api/ecommerce/';
+		XMLDataFormatter::$api_base = 'api/ecommerce/v1/';
 		if(!isset($this->urlParams['ClassName'])) return $this->notFound();
 		$className = $this->urlParams['ClassName'];
 		$id = (isset($this->urlParams['ID'])) ? $this->urlParams['ID'] : null;
 		$relation = (isset($this->urlParams['Relation'])) ? $this->urlParams['Relation'] : null;
-		
+
 		// Check input formats
 		if(!class_exists($className)) return $this->notFound();
 		if($id && !is_numeric($id)) return $this->notFound();
 		if($relation && !preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $relation)) return $this->notFound();
-		
+
 		// fix
 		if($id) {
 			$obj = DataObject::get_by_id($className, $id);
 			$className = $this->urlParams['ClassName'] = $obj->ClassName;
 		}
-		
+
 		// if api access is disabled, don't proceed
 		$apiAccess = singleton($className)->stat('api_access');
 		if(!$apiAccess) return $this->permissionFailure();
