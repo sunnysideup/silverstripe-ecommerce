53c359a5c1bcff620c34d8486f0aaf3400e8ea38
Nicolaas <github@sunnysideup.co.nz>
2012-07-22 05:50:42 +0000

    MINOR
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@3066 950b781c-c544-efab-2f99-90083ee69683

--- code/modifiers/FlaxTaxModifier.php
+++ code/modifiers/FlaxTaxModifier.php
@@ -1,24 +1,20 @@
 <?php
+
 /**
  * Handles calculation of sales tax on Orders.
+ * This is a very basic class that is more like an example
+ * than a workable class.
  *
  * If you would like to make your own tax calculator,
  * create a subclass of this and enable it by using
  * {@link Order::set_modifiers()} in your project
  * _config.php file.
  *
- * Sample configuration in your _config.php:
  *
- * <code>
- *	//rate , name, isexclusive
- * 	FlatTaxModifier::set_tax(0.15, 'GST', false);
- * </code>
- *
- * @authors: Silverstripe, Jeremy, Nicolaas
- *
+ * @authors: Nicolaas [at] Sunny Side Up .co.nz
  * @package: ecommerce
- * @sub-package: modifiers
- *
+ * @sub-package: model
+ * @inspiration: Silverstripe Ltd, Jeremy
  **/
 
 class FlatTaxModifier extends OrderModifier {
