d5017c91c80dbe9e7952f6bf7ee9ed2298d09e86
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-08-16 08:59:15 +0000

    ENHANCEMENT: better address management at checkout (JS)
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@3159 950b781c-c544-efab-2f99-90083ee69683

--- javascript/EcomSelectOrderAddressField.js
+++ javascript/EcomSelectOrderAddressField.js
@@ -36,6 +36,12 @@ var EcomSelectOrderAddressField = {
 	 */
 	removeLinkSelector: ".noLongerInUse",
 
+	/**
+	 * class used to show that something is being loaded
+	 * @var String
+	 */
+	loadingClass: "loading",
+
 
 	/**
 	 * message shown before an address is removed
@@ -53,35 +59,44 @@ var EcomSelectOrderAddressField = {
 	init: function() {
 		EcomSelectOrderAddressField.setupAddressChanges();
 		EcomSelectOrderAddressField.setupNoLongerInUseLinks();
+
 	},
 
 	setupAddressChanges: function(){
 		jQuery(EcomSelectOrderAddressField.fieldSelector).each(
 			function(i, el){
-				jQuery(el).next(EcomSelectOrderAddressField.addressSelector).hide();
-				jQuery(el).find(EcomSelectOrderAddressField.inputSelector).change(
-					function(e) {
-						jQuery(this).parents(EcomSelectOrderAddressField.fieldSelector).next(EcomSelectOrderAddressField.addressSelector).show();
-						id = jQuery(this).val();
-						var data = EcomSelectOrderAddressField.data[id];
-						if(data) {
-							jQuery.each(
-								data,
-								function(i, n){
-									jQuery("input[name='"+i+"'], select[name='"+i+"']").val(n);
+				jQuery(this).find("input:first").click();
+				//jQuery(el).next(EcomSelectOrderAddressField.addressSelector).hide();
+				jQuery(el).find(EcomSelectOrderAddressField.inputSelector).each(
+					function(i, el) {
+						jQuery(el).change(
+							function(e) {
+								//jQuery(this).parents(EcomSelectOrderAddressField.fieldSelector).next(EcomSelectOrderAddressField.addressSelector).show();
+								id = jQuery(this).val();
+								jQuery(this).closest("ul").children("li").removeClass("selected");
+								jQuery(this).closest("li").addClass("selected");
+								var data = EcomSelectOrderAddressField.data[id];
+								if(data) {
+									jQuery.each(
+										data,
+										function(i, n){
+											jQuery("input[name='"+i+"'], select[name='"+i+"']").val(n);
+										}
+									);
 								}
-							);
-						}
+							}
+						);
 					}
-				);
+				)
 			}
-		)
+		);
 	},
 
 	setupNoLongerInUseLinks: function(){
 		jQuery(EcomSelectOrderAddressField.removeLinkSelector).click(
 			function(e) {
 				e.preventDefault();
+				jQuery(this).addClass(EcomSelectOrderAddressField.loadingClass);
 				var id = jQuery(this).attr("rel");
 				url = jQuery(this).attr("href");
 				jQuery.get(
@@ -90,10 +105,11 @@ var EcomSelectOrderAddressField = {
 						jQuery(".val"+id).addClass("removed");
 						jQuery(".val"+id+" input").remove();
 						jQuery(".val"+id+" label").html(data);
+						jQuery(this).removeClass(EcomSelectOrderAddressField.loadingClass);
 					}
 				);
 			}
-		)
+		);
 	}
 
 
