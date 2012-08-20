e55a203de0b83bd15889d6eba24cf39ff89db06e
Nicolaas <github@sunnysideup.co.nz>
2012-08-20 04:33:44 +0000

    ENHANCEMENT: bugfix - cleaning up image reset task
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@3189 950b781c-c544-efab-2f99-90083ee69683

--- code/tasks/EcommerceProductImageReset.php
+++ code/tasks/EcommerceProductImageReset.php
@@ -16,15 +16,39 @@ class EcommerceProductImageReset extends BuildTask{
 	protected $description = "(1) Checks if the image class is ProductImage, (2) Checks if the image exists and remove if it does not exist. NOTE: it is recommended that you update the file system before you run this task.";
 
 	function run($request){
-		$removeCount = 0;
-		$updateClassCount = 0;
-		$tables = array("Product", "Product_Live", "ProductVariation");
+		$tables = array("ProductGroup", "ProductGroup_Live","Product", "Product_Live", "ProductVariation");
 		foreach($tables as $tableName) {
-			$rows = DB::query("SELECT \"ImageID\" FROM \"$tableName\" WHERE ImageID > 0;");
+			$classErrorCount = 0;
+			$removeCount = 0;
+			$updateClassCount = 0;
+			DB::alteration_message("<h2><strong>CHECKING $tableName:</strong></h2>");
+			$rows = DB::query("SELECT \"ImageID\", \"$tableName\".\"ID\" FROM \"$tableName\" WHERE ImageID > 0;");
 			if($rows) {
 				foreach ($rows as $row) {
 					$remove = false;
-					$image = DataObject::get_by_id("Image", $row["ImageID"]);
+					$classErrorCount += DB::query("
+						SELECT COUNT (\"File\".\"ID\")
+						FROM \"File\"
+						WHERE
+							\"File\".\"ID\" = ".$row["ImageID"]."
+							AND  (
+							 \"ClassName\" = 'Image' OR
+							 \"ClassName\" = 'ProductVariation_Image' OR
+							 \"ClassName\" = ''
+							);
+					")->value();
+					DB::query("
+						UPDATE \"File\"
+						SET \"ClassName\" = 'Product_Image'
+						WHERE
+							\"File\".\"ID\" = ".$row["ImageID"]."
+							AND  (
+							 \"ClassName\" = 'Image' OR
+							 \"ClassName\" = 'ProductVariation_Image' OR
+							 \"ClassName\" = ''
+							);
+					");
+					$image = DataObject::get_by_id("Product_Image", $row["ImageID"]);
 					if(!$image) {
 						$remove = true;
 					}
@@ -33,7 +57,7 @@ class EcommerceProductImageReset extends BuildTask{
 					}
 					if($remove) {
 						$removeCount++;
-						DB::query("UPDATE \"$tableName\" SET \"ImageID\" = 0;");
+						DB::query("UPDATE \"$tableName\" SET \"ImageID\" = 0 WHERE \"$tableName\".\"ID\" = ".$row["ID"]." AND \"$tableName\".\"ImageID\" = ".$row["ImageID"].";");
 					}
 					elseif(!($image instanceOf Product_Image)) {
 						$updateClassCount++;
@@ -41,6 +65,12 @@ class EcommerceProductImageReset extends BuildTask{
 						$image-write();
 					}
 				}
+			}
+			if($classErrorCount) {
+				DB::alteration_message("<strong>$tableName:</strong> there were $classErrorCount files with the wrong class names.  These have been fixed.", "deleted");
+			}
+			else {
+				DB::alteration_message("<strong>$tableName:</strong> there were no files with the wrong class names. ", "created");
 			}
 			if($removeCount) {
 				DB::alteration_message("<strong>$tableName:</strong> Removed $removeCount image(s) from products and variations because they do not exist in the file-system or database", "deleted");
