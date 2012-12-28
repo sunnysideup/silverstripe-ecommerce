94027871532710bcdfe23c7b6254811b98e1b35c
Nicolaas <github@sunnysideup.co.nz>
2012-12-28 09:31:47 +0000

    UPGRADE TO 3.0
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/3.0@3798 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/ProductGroupSearchPage.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * This page manages searching for products
+ *
+ * @authors: Nicolaas [at] Sunny Side Up .co.nz
+ * @package: ecommerce
+ * @sub-package: Pages
+ **/
+
+
+class ProductGroupSearchPage extends ProductGroup {
+
+}
+class ProductGroupSearchPage_Controller extends ProductGroup_Controller {
+
+
+}
