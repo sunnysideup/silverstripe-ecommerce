cfb977915614aea60a27786c3398d229e38e879b
Nicolaas <github@sunnysideup.co.nz>
2013-01-28 09:08:44 +0000

    UPGRADE 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/3.0@3902 950b781c-c544-efab-2f99-90083ee69683

--- code/ProductGroupSearchPage.php
+++ code/ProductGroupSearchPage.php
@@ -11,6 +11,25 @@
 
 class ProductGroupSearchPage extends ProductGroup {
 
+	/**
+	 * standard SS variable
+	 * @static String | Array
+	 *
+	 */
+	public static $icon = 'ecommerce/images/icons/productsearch';
+
+	/**
+	 * Standard SS variable.
+	 */
+	public static $singular_name = "Product Search Page";
+		function i18n_singular_name() { return _t("ProductGroupSearchPage.SINGULARNAME", "Product Search Page");}
+
+	/**
+	 * Standard SS variable.
+	 */
+	public static $plural_name = "Product Search Pages";
+		function i18n_plural_name() { return _t("ProductGroupSearchPage.PLURALNAME", "Product Search Pages");}
+
 }
 class ProductGroupSearchPage_Controller extends ProductGroup_Controller {
 
