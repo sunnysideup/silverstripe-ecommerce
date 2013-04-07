37bddd3ed0d71642906244d7e2f3b8f151ca3990
astuart.pett@gmail.com <astuart.pett@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-04-07 05:13:39 +0000

    BUGFIX: Remove double up of copyorder in code/CartPage.php
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/3.0@4179 950b781c-c544-efab-2f99-90083ee69683

--- code/CartPage.php
+++ code/CartPage.php
@@ -559,19 +559,7 @@ class CartPage_Controller extends Page_Controller{
 		return $viewingRealCurrentOrder;
 	}
 
-	/**
-	 * copies either the current order into the shopping cart
-	 *
-	 * TO DO: untested
-	 * TO DO: what to do with old order
-	 *
-	 */
-	function copyorder() {
-		self::set_message(_t("CartPage.ORDERLOADED", "Order has been loaded."));
-		ShoppingCart::singleton()->copyOrder($this->currentOrder->ID);
-		Director::redirect(CheckoutPage::find_last_step_link());
-		return array();
-	}
+
 
 	/**
 	 * @var Boolean
