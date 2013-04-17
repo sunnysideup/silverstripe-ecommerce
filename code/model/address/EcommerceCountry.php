5508f870b3c1a9f412e1bcbf4060f219cf82d265
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-04-17 02:19:00 +0000

    BUGFIX, replacing function with static function for get_country_from_ip
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@4228 950b781c-c544-efab-2f99-90083ee69683

--- code/model/address/EcommerceCountry.php
+++ code/model/address/EcommerceCountry.php
@@ -129,7 +129,7 @@ class EcommerceCountry extends DataObject {
 	 * this is some sort of IP recogniser system (e.g. Geoip Class)
 	 * @return String (country code)
 	 **/
-	public function get_country_from_ip(){
+	public static function get_country_from_ip(){
 		$visitorCountryProviderClassName = EcommerceConfig::get('EcommerceCountry', 'visitor_country_provider');
 		$visitorCountryProvider = new $visitorCountryProviderClassName();
 		return $visitorCountryProvider->getCountry();
