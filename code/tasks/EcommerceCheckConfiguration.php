fbcbe43fc2176532e25ba3731f720e3c8dd98594
Nicolaas <github@sunnysideup.co.nz>
2013-07-11 22:23:18 +0000

    Upgrade to 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/branches/3.1@4613 950b781c-c544-efab-2f99-90083ee69683

--- code/tasks/EcommerceCheckConfiguration.php
+++ code/tasks/EcommerceCheckConfiguration.php
@@ -366,7 +366,7 @@ class EcommerceCheckConfiguration extends BuildTask{
 	protected function addOtherValuesToConfigs(){
 
 		$this->definitions["Email"]["admin_email_address"] = "Default administrator email. <br />SET USING Email::\$admin_email_address = \"bla@ta.com\" in the _config.php FILES";
-		$this->configs["Email"]["admin_email_address"] = Email::$admin_email_address;
+		$this->configs["Email"]["admin_email_address"] = Config::inst()->get("Email", "admin_email_address");
 		$this->defaults["Email"]["admin_email_address"] = "[no default set]";
 
 		$siteConfig = SiteConfig::current_site_config();
