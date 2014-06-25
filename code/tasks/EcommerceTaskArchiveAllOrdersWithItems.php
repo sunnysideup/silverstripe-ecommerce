4699202426b65316130e712fd90032d0b2da06a6
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-06-25 04:29:19 +0000

    better archiving task
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@5050 950b781c-c544-efab-2f99-90083ee69683

--- code/tasks/EcommerceTaskArchiveAllOrdersWithItems.php
+++ code/tasks/EcommerceTaskArchiveAllOrdersWithItems.php
@@ -14,10 +14,12 @@
 
 class EcommerceTaskArchiveAllOrdersWithItems extends BuildTask{
 
-	protected $title = "Archive all orders with order items";
+	protected $title = "Archive all orders with order items and adding a submit record.";
 
 	protected $description = "
-	This task moves all orders to the 'Archived' (last) Order Step without running any of the tasks in between.";
+	This task moves all orders to the 'Archived' (last) Order Step without running any of the tasks in between.
+	NB: It also adds a submit record.
+	";
 
 	function run($request){
 		//IMPORTANT!
@@ -50,5 +52,58 @@ class EcommerceTaskArchiveAllOrdersWithItems extends BuildTask{
 		else {
 			DB::alteration_message("Could not find the last order step.", "deleted");
 		}
+		$this->createSubmissionLogForArchivedOrders();
 	}
+
+	protected function createSubmissionLogForArchivedOrders(){
+		$lastOrderStep = OrderStep::get()->sort("Sort", "DESC")->First();
+		$submissionLogClassName = EcommerceConfig::get("OrderStatusLog", "order_status_log_class_used_for_submitting_order");
+
+		$offSet = 0;
+		$orders = Order::get()->filter(array("StatusID" => $lastOrderStep->ID))->limit(100, $offSet);
+
+		while($orders->count()) {
+			foreach($orders as $order) {
+
+				$isSubmitted = $submissionLogClassName::get()
+					->Filter(array("OrderID" => $order->ID))
+					->count();
+				if(!$isSubmitted) {
+					$obj = $className::create();
+					if(is_a($obj, Object::getCustomClass("OrderStatusLog"))) {
+						//save versions
+						//@todo: test and implement
+						/*
+						if($this->Attributes()->exists()){
+							foreach($this->Attributes() as $attribute){
+								if(is_a($attribute, Object::getCustomClass("OrderItem"))){
+									$buyable = $attribute->Buyable();
+									if(isset($buyable->Version)) {
+										$attribute->Version = $buyable->Version;
+										$attribute->write();
+									}
+								}
+							}
+						}
+						*/
+						$obj->OrderID = $order->ID;
+						//it is important we add this here so that we can save the 'submitted' version.
+						//this is particular important for the Order Item Links.
+						$obj->write();
+						$obj->OrderAsHTML = $order->ConvertToHTML();
+						$obj->write();
+					}
+					else {
+						user_error('EcommerceConfig::get("OrderStatusLog", "order_status_log_class_used_for_submitting_order") refers to a class that is NOT an instance of OrderStatusLog');
+					}
+				}
+			}
+			$offSet += 100;
+			$orders = Order::get()->filter(array("StatusID" => $lastOrderStep->ID))->limit(100, $offSet);
+		}
+
+
+
+	}
+
 }
