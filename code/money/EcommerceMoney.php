fc93b94e2eda119c7f6e5418265cdc15e56f07bd
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2015-02-10 22:56:48 +0000

    adding extra money format
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@5333 950b781c-c544-efab-2f99-90083ee69683

--- code/money/EcommerceMoney.php
+++ code/money/EcommerceMoney.php
@@ -2,11 +2,24 @@
 
 class EcommerceMoney extends Extension {
 
+	/**
+	 * returns the symbol for a currency, e.g. $
+	 * @param String $currency
+	 *
+	 * @return String
+	 */
 	public static function get_default_symbol($currency) {
 		$money = Money::create();
 		return $money->getSymbol($currency);
 	}
 
+	/**
+	 * returns the short symbol for a currency
+	 * This is shorter than the default one.
+	 * @param String $currency
+	 *
+	 * @return String
+	 */
 	public static function get_short_symbol($currency) {
 		$symbol = self::get_default_symbol($currency);
 		if($symbol) {
@@ -18,6 +31,12 @@ class EcommerceMoney extends Extension {
 		}
 	}
 
+	/**
+	 * returns the long symbol for a currency
+	 * @param String $currency
+	 *
+	 * @return String
+	 */
 	public static function get_long_symbol($currency) {
 		$symbol = self::get_default_symbol($currency);
 		if($symbol && mb_strlen($symbol) < 3) {
@@ -26,10 +45,24 @@ class EcommerceMoney extends Extension {
 		return $symbol;
 	}
 
+	/**
+	 * returns the default symbol for a site.
+	 * with or without html
+	 * @param Boolean $html
+	 *
+	 * @return String
+	 */
 	function NiceDefaultSymbol($html = true) {
 		return self::get_default_symbol($this->owner->currency) == self::get_short_symbol($this->owner->currency) ? $this->NiceShortSymbol($html) : $this->NiceLongSymbol($html);
 	}
 
+	/**
+	 * returns the short symbol for a site.
+	 * with or without html
+	 * @param Boolean $html
+	 *
+	 * @return String
+	 */
 	function NiceShortSymbol($html = true) {
 		$symbol = self::get_short_symbol($this->owner->currency);
 		if($html) {
@@ -39,6 +72,13 @@ class EcommerceMoney extends Extension {
 		return (is_numeric($amount)) ? $this->owner->currencyLib->toCurrency($amount, array('symbol' => $symbol, 'display' => Zend_Currency::USE_SYMBOL)) : '';
 	}
 
+	/**
+	 * returns the long symbol for a site.
+	 * with or without html
+	 * @param Boolean $html
+	 *
+	 * @return String
+	 */
 	function NiceLongSymbol($html = true) {
 		$symbol = self::get_long_symbol($this->owner->currency);
 		$short = self::get_short_symbol($this->owner->currency);
@@ -53,11 +93,36 @@ class EcommerceMoney extends Extension {
 		return (is_numeric($amount)) ? $this->owner->currencyLib->toCurrency($amount, array('symbol' => $symbol, 'display' => Zend_Currency::USE_SYMBOL)) : '';
 	}
 
+	/**
+	 * returns a currency like this: 8,001.00 USD / 12.12 NZD
+	 *
+	 * @param Boolean $html
+	 *
+	 * @return String
+	 */
+	public function SymbolNumberAndCode($html = true){
+		$symbol = self::get_short_symbol($this->owner->currency);
+		if($html) {
+			$symbol = "<span class=\"currencySymbol\">$symbol</span>";
+		}
+		$code = strtolower($this->owner->currency);
+		if($html) {
+			$code = "<span class=\"currencyHolder\">$code</span>";
+		}
+		$amount = $this->owner->getAmount();
+		return (is_numeric($amount)) ?  $symbol.$this->owner->currencyLib->toCurrency($amount, array("symbol" => "")).$code : '';
+	}
+
+	/**
+	 * returns the default format for a site for currency
+	 *
+	 * @param Boolean $html
+	 *
+	 * @return String
+	 */
 	function NiceDefaultFormat($html = true) {
 		$function = EcommerceConfig::get('EcommerceMoney', 'default_format');
 		return $this->owner->$function($html);
 	}
-
-
 
 }
