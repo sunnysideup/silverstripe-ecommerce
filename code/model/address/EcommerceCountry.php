8fda16546da1d4d1b0afd77eef9e13bfc23a5a00
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2015-03-19 03:50:52 +0000

    cleanup
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@5409 950b781c-c544-efab-2f99-90083ee69683

--- code/model/address/EcommerceCountry.php
+++ code/model/address/EcommerceCountry.php
@@ -528,28 +528,3 @@ class EcommerceCountry extends DataObject implements EditableEcommerceObject {
 
 }
 
-/**
- * this is a very basic class with as its sole purpose providing
- * the country of the customer.
- * By default we are using the GEOIP class
- * but you can switch it to your own system by changing
- * the classname in the ecommerce.yml config file.
- *
- *
- */
-class EcommerceCountry_VisitorCountryProvider extends Object implements EcommerceGEOipProvider {
-
-	/**
-	 *
-	 * @return String (Country Code - e.g. NZ, AU, or AF)
-	 */
-	public function getCountry() {
-		if(class_exists("Geoip")) {
-			return Geoip::visitor_country();
-		}
-		else {
-			return Config::inst()->get("EcommerceCountry", "default_country_code");
-		}
-	}
-
-}
