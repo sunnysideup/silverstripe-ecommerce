fe886079c26418a1fe6a0a3d2c7f012aadd7633f
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2015-05-28 04:46:29 +0000

    BUGFIX
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/trunk@5513 950b781c-c544-efab-2f99-90083ee69683

--- code/control/ShoppingCart_Controller.php
+++ code/control/ShoppingCart_Controller.php
@@ -612,7 +612,7 @@ class ShoppingCart_Controller extends Controller implements Flushable {
 	 */
 	protected static function params_to_get_string(Array $array){
 		$token = SecurityToken::inst();
-		if(isset($array["SecurityID"])) {
+		if(!isset($array["SecurityID"])) {
 			$array["SecurityID"] = $token->getValue();
 		}
 		return "?".http_build_query($array);
