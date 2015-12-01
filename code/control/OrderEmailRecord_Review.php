3745ecc2cfd1eeac0231ac6eb050e6baf1f34288
Sunny Side Up <github@sunnysideup.co.nz>
2015-12-01 17:47:16 +1300

    bugfixes and smarter dev emails and allowing shopadmin to place orders on behalf of

---
+++ code/control/OrderEmailRecord_Review.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class OrderEmailRecord_Review extends Controller {
+
+	private static $allowed_actions = array(
+		"read" => "ShopAdmin"
+	);
+
+	public static function review_link($email) {
+		return Config::inst()->get("OrderEmailRecord_Review", "url_segment")."/read/".$email->ID;
+	}
+
+	function read($request) {
+		$id = intval($request->param("ID"));
+		$email = OrderEmailRecord::get()->byID($id);
+		return $email->Content;
+	}
+}
--- code/model/process/OrderEmailRecord.php
+++ code/model/process/OrderEmailRecord.php
@@ -217,19 +217,3 @@ class OrderEmailRecord extends DataObject implements EditableEcommerceObject{
 
 }
 
-class OrderEmailRecord_Review extends Controller {
-
-	private static $allowed_actions = array(
-		"read" => "ShopAdmin"
-	);
-
-	public static function review_link($email) {
-		return Config::inst()->get("OrderEmailRecord_Review", "url_segment")."/read/".$email->ID;
-	}
-
-	function read($request) {
-		$id = intval($request->param("ID"));
-		$email = OrderEmailRecord::get()->byID($id);
-		return $email->Content;
-	}
-}
