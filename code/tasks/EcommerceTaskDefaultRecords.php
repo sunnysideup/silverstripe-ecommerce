9e617b3d82ce9fcaefdb9913a911957efc52b7c6
Sunny Side Up <github@sunnysideup.co.nz>
2016-02-02 15:22:52 +1300

    MINOR

--- code/tasks/EcommerceTaskDefaultRecords.php
+++ code/tasks/EcommerceTaskDefaultRecords.php
@@ -110,7 +110,7 @@ class EcommerceTaskDefaultRecords extends BuildTask {
 			if(!$ecommerceConfig->ReceiptEmail) {
 				$ecommerceConfig->ReceiptEmail = Email::config()->admin_email;
 				if(!$ecommerceConfig->ReceiptEmail) {
-					user_error("you must set an AdminEmail (Email::setAdminEmail)", E_USER_NOTICE);
+					user_error("you must set an Admin Email ... Config::inst()->update('Email', 'admin_email', 'foo@bar.nz') ... ", E_USER_NOTICE);
 				}
 				$update[]= "created default entry for ReceiptEmail";
 			}
