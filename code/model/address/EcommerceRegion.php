dce4182ded370576d02855e4a10e39e1d247f916
Sunny Side Up <github@sunnysideup.co.nz>
2016-04-04 14:04:55 +1200

    BUG: typo

--- code/model/address/EcommerceRegion.php
+++ code/model/address/EcommerceRegion.php
@@ -253,7 +253,7 @@ class EcommerceRegion extends DataObject implements EditableEcommerceObject {
 	 * @param array $a
 	 * @param int $orderID
 	 */
-	public static function set_for_current_order_only_show_regions(Array $a, $orderID = 0, ) {
+	public static function set_for_current_order_only_show_regions(Array $a, $orderID = 0) {
 		//We MERGE here because several modifiers may limit the countries
 		$previousArray = self::get_for_current_order_only_show_regions($orderID);
 		self::$_for_current_order_only_show_regions[$orderID] = array_intersect($a, $previousArray);
