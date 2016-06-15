c989cd65b64b7ff7d3778e3f24cb75c1d399fafd
Sunny Side Up <github@sunnysideup.co.nz>
2016-06-15 12:17:49 +1200

    fixes

--- code/Product.php
+++ code/Product.php
@@ -1065,17 +1065,24 @@ class Product extends Page implements BuyableModel
         if ($extended !== null) {
             return $extended;
         }
-        if (!EcommerceCountry::allow_sales()) {
+        if ( ! EcommerceCountry::allow_sales()) {
+            print_r($extended);
+            die("asdfdseeeee");
+
             return false;
         }
 
         $price = $this->getCalculatedPrice();
         if ($price == 0 && !$config->AllowFreeProductPurchase) {
+            print_r($extended);
+            die("asdfdseeeeeaseeeeeeeeeee");
             return false;
         }
         // Standard mechanism for accepting permission changes from decorators
         $extended = $this->extendedCan(__FUNCTION__, $member);
         if ($extended !== null) {
+            print_r($extended);
+            die("asdfdseeeeeooooooooooooooo");
             return $extended;
         }
         return $allowpurchase;
