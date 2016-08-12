c23052fe99fc909032a56ee3696681ee4882f9d9
Sunny Side Up <github@sunnysideup.co.nz>
2016-08-12 14:11:51 +1200

    fixes

--- code/model/OrderItem.php
+++ code/model/OrderItem.php
@@ -657,7 +657,7 @@ class OrderItem extends OrderAttribute
         $tempBuyableStoreType = $current ? 'current' : 'version';
         if (!isset($this->tempBuyableStore[$tempBuyableStoreType])) {
             if (!$this->BuyableID) {
-                //debug::log('There was an error retrieving the product');
+                debug::log('There was an error retrieving the product');
                 return Product::create();
                 return;
             }
