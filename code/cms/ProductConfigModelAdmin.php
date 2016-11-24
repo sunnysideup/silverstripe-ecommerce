21c814e4c706e9fda436f39f522dfa5b4cfa14e7
Sunny Side Up <github@sunnysideup.co.nz>
2016-11-25 12:05:42 +1300

    MINOR: moving file

---
+++ code/cms/ProductConfigModelAdmin.php
@@ -0,0 +1,44 @@
+<?php
+
+
+/**
+ * @description: Manages stuff related to products,
+ * but not the product (groups) themselves
+ *
+ * Main example is product variations
+ *
+ * @authors: Nicolaas [at] Sunny Side Up .co.nz
+ * @package: ecommerce
+ * @sub-package: cms
+ **/
+class ProductConfigModelAdmin extends ModelAdminEcommerceBaseClass
+{
+    private static $menu_priority = 3.19;
+
+    private static $url_segment = 'product-config';
+
+    private static $menu_title = 'Product Details';
+
+    /**
+     * standard SS variable.
+     *
+     * @var string
+     */
+    private static $menu_icon = 'ecommerce/images/icons/product-file.gif';
+
+    public function getEditForm($id = null, $fields = null)
+    {
+        $form = parent::getEditForm();
+        if ($this->modelClass == 'SearchHistory') {
+            if ($gridField = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass))) {
+                $form->Fields()->replaceField(
+                    $gridField->getName(),
+                    EcommerceSearchHistoryFormField::create('SearchHistoryTable')
+                        ->setShowMoreLink(true)
+                );
+            }
+        }
+
+        return $form;
+    }
+}
