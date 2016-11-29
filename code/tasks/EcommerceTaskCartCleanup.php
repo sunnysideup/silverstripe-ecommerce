fb269a77f73956c48dd4eb2ab6b39f863c43e7a5
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2016-11-30 12:26:54 +1300

    MINOR: improved removal of abandonned carts

--- code/tasks/EcommerceTaskCartCleanup.php
+++ code/tasks/EcommerceTaskCartCleanup.php
@@ -62,46 +62,68 @@ class EcommerceTaskCartCleanup extends BuildTask
             DB::alteration_message("<h2>deleting empty and abandonned carts (total cart count = $countAll)</h2>.");
         }
 
-        //ABANDONNED CARTS
-        $clearMinutes = EcommerceConfig::get('EcommerceTaskCartCleanup', 'clear_minutes');
+        $neverDeleteIfLinkedToMember = EcommerceConfig::get('EcommerceTaskCartCleanup', 'never_delete_if_linked_to_member');
         $maximumNumberOfObjectsDeleted = EcommerceConfig::get('EcommerceTaskCartCleanup', 'maximum_number_of_objects_deleted');
 
+        //LIMITS ...
         if ($this->verbose && $request) {
             $limitFromGetVar = $request->getVar('limit');
             if($limitFromGetVar) {
                 $maximumNumberOfObjectsDeleted = intval($limitFromGetVar);
             }
         }
-        $time = strtotime('-'.$clearMinutes.' minutes');
-        $where = '"StatusID" = '.OrderStep::get_status_id_from_code('CREATED')." AND UNIX_TIMESTAMP(\"Order\".\"LastEdited\") < '$time'";
-        $sort = '"Order"."Created" ASC';
         $limit = '0, '.$maximumNumberOfObjectsDeleted;
-        $neverDeleteIfLinkedToMember = EcommerceConfig::get('EcommerceTaskCartCleanup', 'never_delete_if_linked_to_member');
+
+        //sort
+        $sort = '"Order"."Created" ASC';
+
+        //join
+        $leftMemberJoin = 'LEFT JOIN Member ON "Member"."ID" = "Order"."MemberID"';
+        $joinShort = '"Member"."ID" = "Order"."MemberID"';
+
+        //ABANDONNED CARTS
+        $clearMinutes = EcommerceConfig::get('EcommerceTaskCartCleanup', 'clear_minutes');
+        $createdStepID = OrderStep::get_status_id_from_code('CREATED');
+        $time = strtotime('-'.$clearMinutes.' minutes');
+        $where = '"StatusID" = '.$createdStepID." AND UNIX_TIMESTAMP(\"Order\".\"LastEdited\") < '$time'";
         if ($neverDeleteIfLinkedToMember) {
-            $where .= ' AND "Member"."ID" IS NULL';
+            $userStatement = 'or have a user associated with it';
+            $withoutMemberWhere = ' AND "Member"."ID" IS NULL ';
+            $withMemberWhere = ' OR "Member"."ID" IS NOT NULL ';
             $memberDeleteNote = '(Carts linked to a member will NEVER be deleted)';
         } else {
+            $userStatement = '';
+            $withoutMemberWhere = '  ';
+            $withMemberWhere = '';
             $memberDeleteNote = '(We will also delete carts in this category that are linked to a member)';
         }
         $oldCarts = Order::get()
-            ->where($where)
+            ->where($where.$withoutMemberWhere)
             ->sort($sort)
             ->limit($maximumNumberOfObjectsDeleted);
-        $leftMemberJoin = '';
+        $oldCarts = $oldCarts->leftJoin('Member', $joinShort);
         if ($neverDeleteIfLinkedToMember) {
-            $oldCarts = $oldCarts->leftJoin('Member', '"Member"."ID" = "Order"."MemberID"');
-            $leftMemberJoin = 'LEFT JOIN Member ON "Member"."ID" = "Order"."MemberID"';
         }
         if ($oldCarts->count()) {
             $count = 0;
             if ($this->verbose) {
                 $this->flush();
-                $totalToDeleteSQLObject = DB::query("SELECT COUNT(*) FROM \"Order\" $leftMemberJoin WHERE $where");
+                $totalToDeleteSQLObject = DB::query('
+                    SELECT COUNT(*)
+                    FROM "Order"
+                        '.$leftMemberJoin.'
+                    WHERE '
+                        .$where
+                        .$withoutMemberWhere
+                    .';'
+                );
                 $totalToDelete = $totalToDeleteSQLObject->value();
                 DB::alteration_message('
-                    <h2>Total number of abandonned carts: '.$totalToDelete.'</h2>
-                    <br /><b>number of records deleted at one time:</b> '.$maximumNumberOfObjectsDeleted.'
-                    <br /><b>Criteria:</b> last edited '.$clearMinutes.' (~'.round($clearMinutes / 60 / 24, 2)." days) minutes ago or more $memberDeleteNote", 'created');
+                        <h2>Total number of abandonned carts: '.$totalToDelete.'</h2>
+                        <br /><b>number of records deleted at one time:</b> '.$maximumNumberOfObjectsDeleted.'
+                        <br /><b>Criteria:</b> last edited '.$clearMinutes.' (~'.round($clearMinutes / 60 / 24, 2)." days)
+                        minutes ago or more $memberDeleteNote"
+                    , 'created');
             }
             foreach ($oldCarts as $oldCart) {
                 $count++;
@@ -115,42 +137,55 @@ class EcommerceTaskCartCleanup extends BuildTask
         if ($this->verbose) {
             $this->flush();
             $timeLegible = date('Y-m-d H:i:s', $time);
-            $countCart = DB::query('SELECT COUNT("ID") FROM "Order" WHERE "StatusID" = '.OrderStep::get_status_id_from_code('CREATED').' ')->value();
-            $countCartWithinTimeLimit = DB::query('SELECT COUNT("ID") FROM "Order" WHERE "StatusID" = '.OrderStep::get_status_id_from_code('CREATED')." AND UNIX_TIMESTAMP(\"Order\".\"LastEdited\") >= '$time' ")->value();
-            DB::alteration_message("$countCart Orders are still in the CREATED cart state (not submitted), $countCartWithinTimeLimit of them are within the time limit (last edited after $timeLegible) so they are not deleted yet.", 'created');
+            $countCart = DB::query('SELECT COUNT("ID") FROM "Order" WHERE "StatusID" = '.$createdStepID.' ')->value();
+            $countCartWithinTimeLimit = DB::query('
+                SELECT COUNT("Order"."ID")
+                FROM "Order"
+                    '.$leftMemberJoin.'
+                WHERE "StatusID" = '.$createdStepID.'
+                AND
+                (
+                    UNIX_TIMESTAMP("Order"."LastEdited") >= '.$time .'
+                    '.$withMemberWhere.'
+                );
+            ')->value();
+            DB::alteration_message("
+                    $countCart Orders are still in the CREATED cart state (not submitted),
+                    $countCartWithinTimeLimit of them are within the time limit (last edited after $timeLegible)
+                    ".$userStatement." so they are not deleted."
+                ,
+                'created'
+            );
         }
 
         //EMPTY ORDERS
         $clearMinutes = EcommerceConfig::get('EcommerceTaskCartCleanup', 'clear_minutes_empty_carts');
         $time = strtotime('-'.$clearMinutes.' minutes');
         $where = "\"StatusID\" = 0 AND UNIX_TIMESTAMP(\"Order\".\"LastEdited\") < '$time'";
-        $sort = '"Order"."Created" ASC';
-        $limit = '0, '.$maximumNumberOfObjectsDeleted;
-        $neverDeleteIfLinkedToMember = EcommerceConfig::get('EcommerceTaskCartCleanup', 'never_delete_if_linked_to_member');
-        if ($neverDeleteIfLinkedToMember) {
-            $where .= ' AND "Member"."ID" IS NULL';
-            $memberDeleteNote = '(Carts linked to a member will NEVER be deleted)';
-        } else {
-            $memberDeleteNote = '(We will also delete carts in this category that are linked to a member)';
-        }
         $oldCarts = Order::get()
             ->where($where)
             ->sort($sort)
             ->limit($maximumNumberOfObjectsDeleted);
-        $leftMemberJoin = '';
-        if ($neverDeleteIfLinkedToMember) {
-            $oldCarts = $oldCarts->leftJoin('Member', '"Member"."ID" = "Order"."MemberID"');
-            $leftMemberJoin = 'LEFT JOIN Member ON "Member"."ID" = "Order"."MemberID"';
-        }
+        $oldCarts = $oldCarts->leftJoin('Member', $joinShort);
         if ($oldCarts->count()) {
             $count = 0;
             if ($this->verbose) {
                 $this->flush();
-                $totalToDelete = DB::query("SELECT COUNT(*) FROM \"Order\" $leftMemberJoin WHERE $where")->value();
+                $totalToDelete = DB::query('
+                    SELECT COUNT(*)
+                    FROM "Order"
+                        '.$leftMemberJoin.'
+                    WHERE '
+                        .$where
+                        .$withoutMemberWhere
+                    .';'
+                )->value();
                 DB::alteration_message('
-                    <h2>Total number of empty carts: '.$totalToDelete.'</h2>
-                    <br /><b>number of records deleted at one time:</b> '.$maximumNumberOfObjectsDeleted."
-                    <br /><b>Criteria:</b> there are no order items and the order was last edited $clearMinutes minutes ago $memberDeleteNote", 'created');
+                        <h2>Total number of empty carts: '.$totalToDelete.'</h2>
+                        <br /><b>number of records deleted at one time:</b> '.$maximumNumberOfObjectsDeleted."
+                        <br /><b>Criteria:</b> there are no order items and
+                        the order was last edited $clearMinutes minutes ago $memberDeleteNote"
+                    , 'created');
             }
             foreach ($oldCarts as $oldCart) {
                 ++$count;
@@ -164,9 +199,29 @@ class EcommerceTaskCartCleanup extends BuildTask
         if ($this->verbose) {
             $this->flush();
             $timeLegible = date('Y-m-d H:i:s', $time);
-            $countCart = DB::query('SELECT COUNT("ID") FROM "Order" WHERE "StatusID" = 0 ')->value();
-            $countCartWithinTimeLimit = DB::query("SELECT COUNT(\"ID\") FROM \"Order\" WHERE \"StatusID\" = 0 AND UNIX_TIMESTAMP(\"Order\".\"LastEdited\") <= '$time'")->value();
-            DB::alteration_message("$countCart Orders are without status at all, $countCartWithinTimeLimit are within the time limit (last edited after $timeLegible) so they are not deleted yet.", 'created');
+            $countCart = DB::query('
+                SELECT COUNT("Order"."ID")
+                FROM "Order"
+                    '.$leftMemberJoin.'
+                WHERE "StatusID" = 0 '
+            )->value();
+            $countCartWithinTimeLimit = DB::query('
+                SELECT COUNT("Order"."ID")
+                FROM "Order"
+                    '.$leftMemberJoin.'
+                WHERE "StatusID" = 0 AND
+                (
+                    UNIX_TIMESTAMP("Order"."LastEdited") >= '.$time.'
+                    '.$withMemberWhere.'
+                )'
+            )->value();
+            DB::alteration_message("
+                    $countCart Orders are without status at all,
+                    $countCartWithinTimeLimit are within the time limit (last edited after $timeLegible)
+                    ".$userStatement."so they are not deleted yet."
+                ,
+                'created'
+            );
         }
 
         $oneToMany = EcommerceConfig::get('EcommerceTaskCartCleanup', 'one_to_many_classes');
@@ -248,6 +303,9 @@ class EcommerceTaskCartCleanup extends BuildTask
         /***********************************************
         //CLEANING ONE-TO-MANY
         *************************************************/
+
+        //one order has many other things so we increase the ability to delete stuff
+        $maximumNumberOfObjectsDeleted = $maximumNumberOfObjectsDeleted * 25;
         if ($this->verbose) {
             $this->flush();
             DB::alteration_message('<h2>Checking one-to-many relationships</h2>.');
@@ -303,8 +361,12 @@ class EcommerceTaskCartCleanup extends BuildTask
 
     private function flush()
     {
-        ob_flush();
-        flush();
+        if((php_sapi_name() === 'cli')) {
+            echo "\n";
+        } else {
+            ob_flush();
+            flush();
+        }
     }
 
     private function deleteObject($objectToDelete)
