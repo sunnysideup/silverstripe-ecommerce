ebfc433a6fd4bffff5dee07a06ae6fd581d02ba1
Nicolaas <github@sunnysideup.co.nz>
2018-01-10 21:37:29 +1300

    PATCH: typo fix

--- code/model/process/OrderProcessQueue.php
+++ code/model/process/OrderProcessQueue.php
@@ -465,8 +465,6 @@ class OrderProcessQueue extends DataObject
         foreach($errors as $error) {
             DB::alteration_message(' DELETING ROGUE OrderProcessQueue', 'deleted');
             $error->delete();
-        } else {
-            DB::alteration_message(' There are no NO rogue Orders in the OrderProcessQueue');
         }
     }
 
