da2a241f873f5e9a30d49bb1869a88b212c49765
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2019-11-18 23:18:12 +1300

    PATCH: adding more to converter

--- code/api/OrderConverter.php
+++ code/api/OrderConverter.php
@@ -6,6 +6,16 @@ abstract class OrderConverter extends Object
 
     protected $order = null;
 
+    protected $currencyCode = '';
+
+    protected $billingAddress = null;
+
+    protected $shippingAddress = null;
+
+    protected $modifiers = null;
+
+    protected $orderItems = null;
+
     public function __construct(?Order $order = null)
     {
         parent::__construct();
@@ -17,13 +27,46 @@ abstract class OrderConverter extends Object
         } else {
             user_error('We expect an order here ;-), provided is: '.print_r($order, 1));
         }
+
+        $this->retrieveOrderDetails();
     }
+
+    public function retrieveOrderDetails()
+    {
+        $this->currencyCode = $this->order->CurrencyUsed()->Code;
+        $this->billingAddress = $this->order->BillingAddress();
+        $this->orderItems = $this->order->OrderItems();
+        $this->modifiers = $this->order->Modifiers();
+        if($this->order->IsSeparateShippingAddress()) {
+            $this->shippingAddress = $this->order->ShippingAddress();
+        } else {
+            $this->shippingAddress = $billing;
+        }
+    }
+
 
     public function getOrder()
     {
         return $this->order;
     }
 
+    private $amountsPerModifierType = [];
+
+    public function getAmountForModifierType($type)
+    {
+        if(empty($this->amountsPerModifierType)) {
+            foreach($this->modifiers as $modifier) {
+                if($modifier->Type) {
+                    if(! isset($this->amountsPerModifierType[$modifier->Type])) {
+                        $this->amountsPerModifierType[$modifier->Type] = 0;
+                    }
+                    $this->amountsPerModifierType[$modifier->Type] += $modifier->CalculatedTotal;
+                }
+            }
+        }
+
+        return $this->amountsPerModifierType[$type] ?? 0;
+    }
 
     abstract public function convert();
 
