471387d6dd08851743b365fd41396158289db171
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-05-18 11:31:33 +1200

    PATCH: linting

--- code/cms/dev/EcommerceDatabaseAdmin.php
+++ code/cms/dev/EcommerceDatabaseAdmin.php
@@ -428,21 +428,3 @@ class EcommerceDatabaseAdmin extends TaskRunner
         ");
     }
 }
-
-class DebugView_EcommerceDatabaseAdmin extends DebugView
-{
-    public function writePreOutcome()
-    {
-        echo "<div id='TaskHolder' style=\"background-color: #e8e8e8; border-radius: 15px; margin: 20px; padding: 20px\">";
-    }
-
-    public function writePostOutcome()
-    {
-        echo '</div>';
-    }
-
-    public function writeContent(Controller $controller)
-    {
-        echo $controller->RenderWith($controller->class);
-    }
-}
