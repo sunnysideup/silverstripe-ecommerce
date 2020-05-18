83f7b690a76496672666c546c05646f61a7b1c6f
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-05-18 12:00:21 +1200

    PATCH: linting

---
+++ code/api/EcommerceCountry_VisitorCountryProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * this is a very basic class with as its sole purpose providing
+ * the country of the customer.
+ * By default we are using the GEOIP class
+ * but you can switch it to your own system by changing
+ * the classname in the ecommerce.yml config file.
+ */
+class EcommerceCountryVisitorCountryProvider extends Object implements EcommerceGEOipProvider
+{
+    /**
+     * @return string (Country Code - e.g. NZ, AU, or AF)
+     */
+    public function getCountry()
+    {
+        if (class_exists('Geoip')) {
+            return Geoip::visitor_country();
+        }
+        return Config::inst()->get('EcommerceCountry', 'default_country_code');
+    }
+
+    /**
+     * returns string of IP address.
+     */
+    public function getIP()
+    {
+        return Controller::curr()->getRequest()->getIP();
+    }
+}
