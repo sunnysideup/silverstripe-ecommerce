b122b83bbaa61968a7f353278960062789276221
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-05-18 13:56:37 +1200

    PATCH: linting

---
+++ code/tasks/EcommerceTaskCartManipulationDebug.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+/**
+ * shows you the link to remove the current cart.
+ *
+ * @authors: Nicolaas
+ * @package: ecommerce
+ * @sub-package: tasks
+
+ **/
+class EcommerceTaskCartManipulationDebug extends BuildTask
+{
+    protected $title = 'Show debug links';
+
+    protected $description = 'Use a bunch of debug links to work with various objects such as the cart, the product group and the product page.';
+
+    public function run($request)
+    {
+        $myProductGroup = DataObject::get_one('ProductGroup');
+        $myProduct = DataObject::get_one('Product');
+        $html = '
+        Please use the links below:
+        <ul>
+            <li><a href="/shoppingcart/debug/" target="_debug">debug cart</a></li>
+            <li><a href="/shoppingcart/ajaxtest/?ajax=1" target="_debug">view cart response</a></li>';
+        if ($myProductGroup) {
+            $html .= '
+            <li><a href="' . $myProductGroup->Link('debug') . '" target="_debug">debug product group</a></li>';
+        }
+        if ($myProduct) {
+            $html .= '
+            <li><a href="' . $myProduct->Link('debug') . '" target="_debug">debug product</a></li>';
+        }
+        $html .= '
+        </ul>';
+        DB::alteration_message("${html}");
+    }
+}
