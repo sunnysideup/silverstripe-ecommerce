1e7e2b3d58b3b843e794510a5b4a472b9006d7ba
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-05-18 13:31:13 +1200

    PATCH: linting

---
+++ code/model/address/EcommerceRegionVisitorRegionProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+class EcommerceRegionVisitorRegionProvider extends Object
+{
+    /**
+     * @return int - region ID
+     */
+    public function getRegion()
+    {
+        $region = DataObject::get_one('EcommerceRegion');
+        if ($region) {
+            return $region->ID;
+        }
+    }
+}
