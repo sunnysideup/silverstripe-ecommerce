c217e1a5d4ea0fe52189b90ab9fb556b0b8cb813
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-05-18 12:35:25 +1200

    PATCH: linting

--- code/model/OrderAttribute.php
+++ code/model/OrderAttribute.php
@@ -337,7 +337,8 @@ class OrderAttribute extends DataObject implements EditableEcommerceObject
         $class = static::class;
         $classes = [];
         $classes[] = strtolower($class);
-        while (get_parent_class($class) !== 'DataObject' && $class = get_parent_class($class)) {
+        $class = get_parent_class($class);
+        while ($class && get_parent_class($class) !== 'DataObject') {
             $classes[] = strtolower($class);
         }
         if (is_a($this, Object::getCustomClass('OrderItem'))) {
