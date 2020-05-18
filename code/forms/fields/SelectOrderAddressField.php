2fcaa1d9e95dc541983d5da74edea7e087eb6ded
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-05-18 12:25:28 +1200

    PATCH: linting

--- code/forms/fields/SelectOrderAddressField.php
+++ code/forms/fields/SelectOrderAddressField.php
@@ -44,7 +44,7 @@ class SelectOrderAddressField extends OptionsetField
      */
     public function Field($properties = [])
     {
-        $jsArray = [];
+        // $jsArray = [];
         $js = '
             if(typeof EcomSelectOrderAddressFieldOptions === "undefined") {
                 EcomSelectOrderAddressFieldOptions = [];
