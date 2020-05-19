071b897aeaca1894376bdf651ef413a7dd28da41
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-05-19 16:55:09 +1200

    PATCH: cleanup

---
+++ docs/en/examples/EcomCartConfigExample.js
@@ -0,0 +1,66 @@
+EcomCartOptions.shoppingCartURLSegment = '';
+EcomCartOptions.ajaxCountryFieldSelector = '';
+EcomCartOptions.ajaxRegionFieldSelector = '';
+EcomCartOptions.selectorMainCountryField = '';
+EcomCartOptions.classToShowLoading = '';
+EcomCartOptions.showClass = '';
+EcomCartOptions.hideClass = '';
+EcomCartOptions.selectorShowOnZeroItems = '';
+EcomCartOptions.selectorItemRows = '';
+EcomCartOptions.removeCartSelector = '';
+EcomCartOptions.ajaxButtonsOn = '';
+EcomCartOptions.confirmDeleteText = '';
+EcomCartOptions.ajaxLinksAreaSelector = '';
+EcomCartOptions.addLinkSelector = '';
+EcomCartOptions.removeLinkSelector = '';
+EcomCartOptions.orderItemHolderSelector = '';
+EcomCartOptions.ajaxLinksAreaSelector = '';
+
+EcomQuantityField.set_delegateRootSelector("");
+EcomQuantityField.set_quantityFieldSelector("");
+EcomQuantityField.set_removeSelector("");
+EcomQuantityField.set_addSelector("");
+EcomQuantityField.set_URLSegmentHiddenFieldSelectorAppendix("");
+
+
+EcomOrderForm.set_TermsAndConditionsMessage("");
+EcomOrderForm.set_TermsAndConditionsMessage("");
+
+EcomOrderFormWithShippingAddress.set_firstNameSelector("");
+EcomOrderFormWithShippingAddress.set_shippingFirstNameSelector("");
+EcomOrderFormWithShippingAddress.set_surnameSelector("");
+EcomOrderFormWithShippingAddress.set_shippingSurnameSelector("");
+EcomOrderFormWithShippingAddress.set_addressSelector("");
+EcomOrderFormWithShippingAddress.set_extraAddressSelector("");
+EcomOrderFormWithShippingAddress.set_shippingAddressSelector("");
+EcomOrderFormWithShippingAddress.set_shippingExtraAddressSelector("");
+EcomOrderFormWithShippingAddress.set_citySelector("");
+EcomOrderFormWithShippingAddress.set_shippingCitySelector("");
+EcomOrderFormWithShippingAddress.set_postalCodeSelector("");
+EcomOrderFormWithShippingAddress.set_shippingPostalCode("");
+EcomOrderFormWithShippingAddress.set_countrySelector("");
+EcomOrderFormWithShippingAddress.set_shippingCountrySelector("");
+EcomOrderFormWithShippingAddress.set_shippingSectionSelector("");
+EcomOrderFormWithShippingAddress.set_useShippingDetailsSelector("");
+
+EcomOrderFormAddress.set_chars("");
+EcomOrderFormAddress.set_stringLength("");
+EcomOrderFormAddress.set_passwordFieldInputSelectors("");
+EcomOrderFormAddress.set_choosePasswordLinkSelector("");
+EcomOrderFormAddress.set_passwordGroupHolderSelector("");
+EcomOrderFormAddress.set_postalCodeLink("");
+EcomOrderFormAddress.set_postalCodeLinkTarget("");
+
+
+EcomSelectOrderAddressField.set_fieldSelector("");
+EcomSelectOrderAddressField.set_removeLinkSelector("");
+EcomSelectOrderAddressField.set_areYouSureMessage("");
+EcomSelectOrderAddressField.set_data([]);
+
+EcomProducts.set_selectVariationSelector("");
+
+EcomPayment.set_paymentInputsSelector("");
+EcomPayment.paymentFieldSelector("");
+EcomPayment.paymentMethodPrefix("");
+
+EcomPrintAndMail.set_selectors("");
