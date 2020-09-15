646cfa6ddd0cc13153c2c6c1d55eef1ababd2800
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-09-15 20:14:49 +1200

    WIP

--- src/Config/EcommerceConfig.php
+++ src/Config/EcommerceConfig.php
@@ -112,32 +112,16 @@ use Spyc;
  * @package: ecommerce
  * @sub-package: configuration
  **/
-class EcommerceConfig
+class EcommerceConfig extends Config
 {
     use Extensible;
     use Injectable;
     use Configurable;
 
-    /**
-     * The location(s) of the .yaml fixture file, relative to the site base dir.
-     *
-     * @var array
-     */
-    private static $folder_and_file_locations = ['ecommerce/_config/ecommerce.yml', 'ecommerce/_config/payment.yml'];
-
-    /**
-     * Fetches value for a class, or a property on that class
-     *
-     * @param string $className Class name to retrieve config for
-     * @param string $identifier Optional class property to get
-     * @param int|true $excludeMiddleware Optional flag of middleware to disable.
-     * Passing in `true` disables all middleware.
-     * Can also pass in int flags to specify specific middlewares.
-     *
-     * @see Config::get()
-     */
     public static function get($className, $identifier, $excludeMiddleware = 0)
     {
-        return Config::inst()->get($className, $identifier, $excludeMiddleware);
+        return self::inst()->get($className, $identifier, $excludeMiddleware);
     }
+
+
 }
