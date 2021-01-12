c9f020084dcaf673ca3650b158bd90c4b07abb97
Natsuki Hasegawa <nat@sunnysideup.co.nz>
2021-01-12 16:49:59 +1300

    PATCH: fix product image

---
+++ src/Tasks/UpdateProductImages.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Sunnysideup\Ecommerce\Tasks;
+
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\ORM\DB;
+
+/**
+ * works out how many products have been sold, per product.
+ *
+ * @TODO: consider whether this does not sit better in its own module.
+ * @TODO: refactor based on new database fields
+ *
+ * @authors: Nicolaas [at] Sunny Side Up .co.nz
+ * @package: ecommerce
+ * @sub-package: tasks
+
+ **/
+class UpdateProductImages extends BuildTask
+{
+    protected $title = 'Sets Product Images to a new ClassName compatible with Ecommerce for Silvertripe 4.0';
+
+    protected $description = 'Changes all Images in database from ClassName ProductImage to ClassName SilverStripe\\Assets\\Image';
+
+    public function run($request)
+    {
+        DB::query('
+            UPDATE "File"
+            SET "ClassName" = \''.Image::class.'\'
+            WHERE
+                "ClassName" =  \'ProductImage\';
+        ');
+
+        echo '<h1>DONE!</h1>';
+
+    }
+}
