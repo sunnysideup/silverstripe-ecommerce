d3e97b9a0f8ee60412269764b492ec35be9e0a7a
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-02-21 21:15:02 +1300

    PATCH: fix

--- src/Cms/EcommerceModelAdminTrait.php
+++ src/Cms/EcommerceModelAdminTrait.php
@@ -12,36 +12,6 @@ use SilverStripe\ORM\DataObject;
 
 trait EcommerceModelAdminTrait
 {
-    /**
-     * @param DataObject $record
-     *
-     * @return Form
-     */
-    public function oneItemForm(DataObject $record)
-    {
-        Config::modify()->update(LeftAndMain::class, 'tree_class', $record->ClassName);
-        $form = LeftAndMain::getEditForm($record);
-        $idField = HiddenField::create('ID')->setValue($record->ID);
-        $cssField = LiteralField::create(
-            'oneItemFormCSS',
-            '
-                <style>
-                    .cms-content-view .ui-tabs-nav {
-                        margin-left: 0!important;
-                    }
-                    .cms-content-view .Actions {
-                        position: fixed;
-                        bottom: 16px;
-                        right:  16px;
-                    }
-                </style>
-            '
-        );
-        $form->Fields()->push(LiteralField::create('EditRecords', '<a href="'.$record->CMSEditLink().'">Edit '.$record->getTitle().'</a>'));
-        $form->Fields()->push($idField);
-        $form->Fields()->push($cssField);
-        return $form;
-    }
 
     /**
      * Define which fields are used in the {@link getEditForm} GridField export.
