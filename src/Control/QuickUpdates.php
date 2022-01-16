9419682291ea32aac0d03d96787824e3fadf75d7
nicolaas @ tappy <github@sunnysideup.co.nz>
2022-01-16 22:28:48 +1300

    WIP

--- src/Control/QuickUpdates.php
+++ src/Control/QuickUpdates.php
@@ -12,6 +12,7 @@ use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\Form;
 use SilverStripe\Forms\FormAction;
+use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\RequiredFields;
 use SilverStripe\ORM\FieldType\DBField;
 use SilverStripe\ORM\FieldType\DBDatetime;
@@ -19,6 +20,10 @@ use SilverStripe\ORM\FieldType\DBDatetime;
 use SilverStripe\ORM\ArrayList;
 use SilverStripe\View\ArrayData;
 use SilverStripe\View\Requirements;
+
+use Sunnysideup\Ecommerce\Pages\Product;
+
+use Level51\AjaxSelectField\AjaxSelectField;
 
 class QuickUpdates extends Controller
 {
@@ -28,15 +33,43 @@ class QuickUpdates extends Controller
         'index' => 'SHOPASSISTANTS',
         'do' => 'SHOPASSISTANTS',
         'done' => 'SHOPASSISTANTS',
+        'list' => 'SHOPASSISTANTS',
+        'updateone' => 'SHOPASSISTANTS',
         'MyForm' => 'SHOPASSISTANTS',
     ];
 
-    public function Title()
+    protected $isList = false;
+
+    protected $currentItemID = 0;
+
+    public function updateone($request)
+    {
+        $this->currentItemID = (int) $request->param('ID');
+        return [];
+    }
+
+    public function list($request)
+    {
+        $this->isList = true;
+        return [];
+    }
+
+    public function IsList() : bool
+    {
+        return $this->isList;
+    }
+
+    public function ListLink() : string
+    {
+        return $this->Link('list');
+    }
+
+    public function Title(): string
     {
         return $this->getTitle();
     }
 
-    public function getTitle()
+    public function getTitle(): string
     {
         return 'E-commerce Quick Updates';
     }
@@ -78,7 +111,7 @@ class QuickUpdates extends Controller
         return $al;
     }
 
-    public function Now()
+    public function Now() : string
     {
         return DBDatetime::now()->Nice();
     }
@@ -98,5 +131,66 @@ class QuickUpdates extends Controller
     {
         parent::init();
         Requirements::javascript('silverstripe/admin: thirdparty/jquery/jquery.js');
+    }
+
+    /**
+     *
+     * @param  string   $name
+     * @param  string   $title
+     * @param  integer  $id
+     * @return FormField
+     */
+    protected function productLookupField(string $name, string $title, ?int $id = 0)
+    {
+        $product = Product::get()->byID($id);
+        if($product) {
+            return DropdownField::create(
+                'ParentProductID',
+                'Product',
+            )
+                ->setSource([$product->ID => $product->FullName])
+                ->setHasEmptyDefault(false)
+                ->setValue($product->ID);
+        } else {
+            $callBackFx = function ($query, $request) {
+                $filter = [
+                    'Title:PartialMatch' => '__QUERY__',
+                    'InternalItemID:PartialMatch' => '__QUERY__',
+                ];
+                $list = Product::get()
+                ->filter(['AllowPurchase' => true,])
+                ->sort('InternalItemID');
+                // This part is only required if the idOnlyMode is active
+                foreach($filter as $key => $value) {
+                    if($query) {
+                        $value = str_replace('__QUERY__', $query, $value);
+                        $filter[$key] = $value;
+                    } else {
+                        unset($filter[$key]);
+                    }
+                }
+                $objects = $list->filterAny($filter)->limit(50);
+                $results = [];
+                foreach ($objects as $obj) {
+                    $results[] = [
+                        'id' => $obj->ID,
+                        'title' => $obj->FullName
+                    ];
+                }
+
+                return $results;
+            };
+            return $this->myLookupField($name, $title, $callBackFx);
+        }
+    }
+
+    protected function myLookupField($name, $title, $callBackFx): AjaxSelectField
+    {
+        return AjaxSelectField::create($name, $title)
+            ->setMinSearchChars(3)
+            ->setPlaceholder('find ...'.$title)
+            ->setIdOnlyMode(true)
+            ->setSearchCallback($callBackFx)
+        ;
     }
 }
