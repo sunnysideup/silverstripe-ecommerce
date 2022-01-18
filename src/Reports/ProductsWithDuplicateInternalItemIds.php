a3450f61d1a0678935c1cd96ee0626bfe038861e
Nicolaas <github@sunnysideup.co.nz>
2022-01-19 12:22:26 +1300

    WIP

---
+++ src/Reports/ProductsWithDuplicateInternalItemIds.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Sunnysideup\Ecommerce\Reports;
+
+use SilverStripe\Reports\Report;
+use Sunnysideup\Ecommerce\Reports\EcommerceProductReportTrait;
+
+use Sunnysideup\Ecommerce\Pages\Product;
+
+/**
+ * Selects all products without an image.
+ *
+ * @authors: Nicolaas [at] Sunny Side Up .co.nz
+ * @package: ecommerce
+ * @sub-package: reports
+ */
+class ProductsWithDuplicateInternalItemIds extends Report
+{
+    use EcommerceProductReportTrait;
+
+    protected $dataClass = Product::class;
+
+    /**
+     * @return int - for sorting reports
+     */
+    public function sort()
+    {
+        return 7001;
+    }
+
+    /**
+     * @return string
+     */
+    public function title()
+    {
+        return 'E-commerce: Products: with duplicate internal item IDs (product codes / skus)';
+    }
+
+    /**
+     * @param mixed $list
+     */
+    protected function updateEcommerceList($list)
+    {
+        return $list
+            ->where('
+                (TheOtherProductTable.ID IS NOT NULL AND TheOtherProductTable.ID <> Product.ID)
+
+                OR RIGHT(Product.InternalItemID,2) = \'_2\'
+                OR RIGHT(Product.InternalItemID,2) = \'_3\'
+                OR RIGHT(Product.InternalItemID,2) = \'_4\'
+            ')
+            ->sort('Title', 'ASC')
+            ->leftJoin(
+                'Product',
+                '"Product"."InternalItemID" = TheOtherProductTable.InternalItemID',
+                'TheOtherProductTable'
+            )
+        ;
+    }
+}
