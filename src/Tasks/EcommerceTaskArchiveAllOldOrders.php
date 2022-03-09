e0511efb9842f7500dec2ff42a1b558401cee8e4
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2022-03-10 11:06:45 +1300

    WIP

---
+++ src/Tasks/EcommerceTaskArchiveAllOldOrders.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Sunnysideup\Ecommerce\Tasks;
+
+use SilverStripe\Control\Email\Email;
+use SilverStripe\Control\Email\Mailer;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\DB;
+use Sunnysideup\Ecommerce\Config\EcommerceConfig;
+use Sunnysideup\Ecommerce\Email\EcommerceDummyMailer;
+use Sunnysideup\Ecommerce\Model\Process\OrderStatusLog;
+use Sunnysideup\Ecommerce\Model\Process\OrderStep;
+
+/**
+ * After a bug in the saving of orders in the CMS
+ * This "fixer"  was introduced to fix older orders
+ * without a submission record.
+ *
+ * @authors: Nicolaas [at] Sunny Side Up .co.nz
+ * @package: ecommerce
+ * @sub-package: tasks
+ */
+class EcommerceTaskArchiveAllOldOrders extends BuildTask
+{
+    protected $title = 'Archive all old orders';
+
+    protected $description = "This task moves all orders to the 'Archived' (last) Order Step that were created ".self::AGO_STATEMENT;
+
+    private const AGO_STATEMENT = '-6 months';
+
+    public function run($request)
+    {
+        //IMPORTANT!
+        Config::modify()->update(Email::class, 'send_all_emails_to', 'no-one@localhost');
+        Injector::inst()->registerService(new EcommerceDummyMailer(), Mailer::class);
+        $orderStatusLogTableName = OrderStatusLog::getSchema()->tableName(OrderStatusLog::class);
+        $lastOrderStep = DataObject::get_one(
+            OrderStep::class,
+            '',
+            $cache = true,
+            ['Sort' => 'DESC']
+        );
+        if ($lastOrderStep) {
+            $whereSQL = 'WHERE "StatusID" <> ' . $lastOrderStep->ID . ' AND UNIX_TIMESTAMP(Created) < '.strtotime(self::AGO_STATEMENT);
+            $count = DB::query("
+                SELECT COUNT (\"Order\".\"ID\")
+                FROM \"Order\"
+                {$whereSQL}
+            ")->value();
+            DB::query("
+                UPDATE \"Order\"
+                SET \"Order\".\"StatusID\" = " . $lastOrderStep->ID . "
+                {$whereSQL}
+            ");
+            if ($count) {
+                DB::alteration_message("NOTE: {$count} records were updated.", 'created');
+            } else {
+                DB::alteration_message('No records were updated.');
+            }
+        } else {
+            DB::alteration_message('Could not find a class name for submitted orders.', 'deleted');
+        }
+    }
+}
