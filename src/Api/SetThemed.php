8d467c466d2010b3953d91db20bf67a3657b8089
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2022-03-24 15:27:50 +1300

    WIP

--- src/Api/SetThemed.php
+++ src/Api/SetThemed.php
@@ -5,14 +5,19 @@ namespace Sunnysideup\Ecommerce\Api;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\View\SSViewer;
 
+use SilverStripe\Versioned\Versioned;
+
 class SetThemed
 {
     protected static $changed = true;
+    protected static $stage = true;
 
     public static function start()
     {
         $themed = Config::inst()->get(SSViewer::class, 'theme_enabled');
+        self::$stage = Versioned::get_stage();
         if (! $themed) {
+            Versioned::set_stage(Versioned::LIVE);
             self::$changed = true;
             Config::nest();
             Config::modify()->update(SSViewer::class, 'theme_enabled', true);
@@ -22,6 +27,7 @@ class SetThemed
     public static function end()
     {
         if (self::$changed) {
+            Versioned::set_stage(self::$stage);
             Config::unnest();
             // Config::modify()->update(SSViewer::class, 'theme_enabled', false);
             self::$changed = false;
