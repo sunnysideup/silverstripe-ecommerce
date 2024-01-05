811c7b4428b8b9cce35fc8dae533b5284a07bea9
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-01-05 16:30:24 +1300

    WIP

--- src/Api/ProductCollection.php
+++ src/Api/ProductCollection.php
@@ -4,6 +4,7 @@ namespace Sunnysideup\Ecommerce\Api;
 
 use SilverStripe\ORM\ArrayList;
 use SilverStripe\ORM\DB;
+use SilverStripe\View\ArrayData;
 
 /**
  * @description: Sometimes you need a large collection of products
@@ -13,26 +14,61 @@ use SilverStripe\ORM\DB;
  */
 abstract class ProductCollection
 {
-    abstract public function getArrayList(): ArrayList;
+    /**
+     * @return ArrayList
+     */
+    public function getArrayList(): ArrayList
+    {
+        $arrayList = ArrayList::create();
 
+        $products = $this->getArrayBasic();
+        foreach ($products as $product) {
+            $arrayList->push(
+                ArrayData::create(
+                    $product
+                )
+            );
+        }
+        return $arrayList;
+    }
+
+    /**
+     * Allows you to extend the array with additional information.
+     * If there is no need to extend getBasicArray method, then just return that.
+     *
+     * @return array
+     */
     abstract public function getArrayFull(): array;
 
+    /**
+     * write like this:
+     * ```php
+     *      $array = [];
+     *      $products = DB::query($this->getSQL());
+     *      foreach ($products as $product) {
+     *          // ensure special chars are converted to HTML entities for XML output
+     *          // do other stuff!
+     *          $array[] = $product;
+     *      }
+     *      return $array;
+     *
+     * @return array
+     */
     public function getArrayBasic(): array
     {
         $array = [];
-
         $products = DB::query($this->getSQL());
-
         foreach ($products as $product) {
-            $array[$product['ProductID']] = $product['ClassName'];
+            // ensure special chars are converted to HTML entities for XML output
+            // do other stuff!
+            $array[] = $product;
         }
-
         return $array;
     }
 
-    public function getSQL(): string
+    public function getSQL(?string $where = ''): string
     {
-        $stage = '_Live';
+        $stage = '_Live'; // always live
 
         return '
             SELECT
@@ -43,6 +79,7 @@ abstract class ProductCollection
             INNER JOIN
                 "Product' . $stage . '" ON "SiteTree' . $stage . '"."ID" = "Product' . $stage . '"."ID"
             WHERE
+                ' . $where . '
                 "Product' . $stage . '"."AllowPurchase" = 1
             ;
         ';
