be9dbbda24b0a70b6eab51f5214f92863d247aea
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-06-27 08:14:52 +1200

    ENH: simplified password field

--- client/javascript/EcomPasswordField.js
+++ client/javascript/EcomPasswordField.js
@@ -4,17 +4,13 @@
  * @todoset up a readonly system
  *
  **/
-if (document.getElementsByClassName('div.password.field.text').length) {
+if (document.getElementsByClassName('passwordToggleLink').length) {
   const EcomPasswordField = {
-    chars: '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz',
-
     passwordFieldInputSelectors: 'div.password.field.text',
 
     errorMessageSelector: 'div.password.field.text span.message',
 
     choosePasswordLinkSelector: '.passwordToggleLink',
-
-    stringLength: 14,
 
     //toggles password selection and enters random password so that users still end up with a password
     //even if they do not choose one.
@@ -47,12 +43,10 @@ if (document.getElementsByClassName('div.password.field.text').length) {
                       .jQuery(EcomPasswordField.passwordFieldInputSelectors)
                       .is(':visible')
                   ) {
-                    var newPassword = ''
                     var newLabel = window
                       .jQuery(EcomPasswordField.choosePasswordLinkSelector)
                       .attr('datano')
                   } else {
-                    var newPassword = EcomPasswordField.passwordGenerator()
                     var newLabel = window
                       .jQuery(EcomPasswordField.choosePasswordLinkSelector)
                       .attr('datayes')
@@ -60,11 +54,6 @@ if (document.getElementsByClassName('div.password.field.text').length) {
                   window
                     .jQuery(EcomPasswordField.choosePasswordLinkSelector)
                     .text(newLabel)
-                  window
-                    .jQuery(EcomPasswordField.passwordFieldInputSelectors)
-                    .each(function (i, el) {
-                      window.jQuery(el).find('input').val(newPassword)
-                    })
                 })
               return false
             }
@@ -101,17 +90,6 @@ if (document.getElementsByClassName('div.password.field.text').length) {
           .jQuery(EcomPasswordField.choosePasswordLinkSelector)
           .trigger('click')
       }
-    },
-
-    //generates random password
-    passwordGenerator: function () {
-      return ''
-      var randomstring = ''
-      for (var i = 0; i < this.stringLength; i++) {
-        var rnum = Math.floor(Math.random() * this.chars.length)
-        randomstring += this.chars.substring(rnum, rnum + 1)
-      }
-      return randomstring
     }
   }
   jQuery(() => {
