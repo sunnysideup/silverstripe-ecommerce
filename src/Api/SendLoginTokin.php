4929afe2e5f10333522efa881afed8a0c700bf26
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2026-01-05 13:48:30 +1300

    API: better login using one time code

--- src/Api/SendLoginTokin.php
+++ src/Api/SendLoginTokin.php
@@ -13,6 +13,7 @@ use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Core\Injector\Injectable;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Security\Member;
+use Sunnysideup\OneTimeCode\OneTimeCodeApi;
 
 class SendLoginToken
 {
@@ -29,71 +30,9 @@ class SendLoginToken
         if (! $request) {
             return false;
         }
-
-        /** @var TokenConfig $config */
-        $config = Injector::inst()->get(TokenConfig::class);
-
-        if ($config->isSameBrowser()) {
-            $token = uniqid('logintoken', true);
-            $cookieDuration = ((1 / 24) / 60) * $config->getTokenLifetime();
-
-            Cookie::set(TokenLoginHandler::SECURITY_TOKEN, $token, $cookieDuration);
-            $request->getSession()->set(TokenLoginHandler::SECURITY_TOKEN, $token);
-        }
-
-        $member = $this->getMemberAndValidate($email);
-        if (! $member) {
-            return false;
-        }
-
-        $member->generateToken();
-        $member->write();
-
-        $signinLink = $this->getSigninLink($member, $backURL);
-
-        Email::create()
-            ->setHTMLTemplate('Firesphere\\MagicLogin\\Email\\TokenLoginEmail')
-            ->setData($member)
-            ->addData('SigninLink', $signinLink)
-            ->setSubject(_t(
-                'Firesphere\\MagicLogin\\Controllers\\TokenLoginHandler.MAILSUBJECT',
-                'Your magic log-in link'
-            ))
-            ->setTo($member->Email)
-            ->send();
-
+        Injector::inst()
+            ->get(OneTimeCodeApi::class)
+            ->SendOneTimeCode(['Email' => $email], $request);
         return true;
-    }
-
-    protected function getSigninLink(Member $member, ?string $backURL = ''): string
-    {
-        $url = sprintf('/Security/login/token/token?token=%s', $member->LoginToken);
-
-        if ($backURL) {
-            $url .= '&BackURL=' . urlencode($backURL);
-        }
-
-        return Director::absoluteURL($url);
-    }
-
-    protected function getMemberAndValidate(string $email): ?Member
-    {
-        $field = Member::config()->get('unique_identifier_field') ?? 'Email';
-
-        /** @var Member|null $member */
-        $member = Member::get()
-            ->filter([$field => $email])
-            ->first();
-
-        if (
-            ! $member ||
-            ! $member->isInDB() ||
-            ! filter_var($member->Email, FILTER_VALIDATE_EMAIL) ||
-            ($member->TokenExpires && $member->TokenExpires > time())
-        ) {
-            return null;
-        }
-
-        return $member;
     }
 }
